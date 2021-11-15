c5231c33019f1e7cc4be6d114211f3849818403a
Mmmr7 <ol-sidorenkov@mail.ru>
2021-11-15 17:19:36 +0300

    comments and fixes

--- main.go
+++ main.go
@@ -9,66 +9,99 @@ import (
 	"time"
 )
 
+// Ebay.
 func GetEbayTags(ask string) {
+	// The goroutine is done.
+	// Defer statements are executed the last.
 	defer wg.Done()
 
+	// Creating url using given request and checking its correctness.
 	var url string = fmt.Sprintf("https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw=%s&_sacat=0", ask)
 	fmt.Println(url)
 
+	// Creating request and checking if it can be opened.
 	res, err := http.Get(url)
 	if err != nil {
 		fmt.Println(1)
 		return
 	}
 
+	// Closing request before leaving the function.
+	// Defer statements are executed the last.
 	defer res.Body.Close()
+
+	// If cannot work with res.
 	if res.StatusCode != 200 {
 		fmt.Println(2)
 		return
 	}
 
+	// Creating page source .html document and checking if it can be created.
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		fmt.Println(3)
 		return
 	}
 
+	// Searching all needed div tags.
 	doc.Find("div[class~=srp-related-searches]").Each(func(i int, s *goquery.Selection) {
+		// Searching all needed span tags inside the div tags.
 		s.Find("span[class=BOLD]").Each(func(i int, ss *goquery.Selection) {
+			// Logging.
 			fmt.Printf("%s ", ss.Text())
 		})
 	})
+
+	// Just a new line.
 	fmt.Println("")
 }
 
+// Wildberries.
 func GetWildberriesTags(ask string) {
+	// The goroutine is done.
+	// Defer statements are executed the last.
 	defer wg.Done()
 
+	// Creating url using given request and checking its correctness.
 	var url string = fmt.Sprintf("https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw=%s&_sacat=0", ask)
 	fmt.Println(url)
 
 	// TODO...
 
+	// Just a new line.
 	fmt.Println("")
 }
 
 func GetTags(ask string) {
+	// Giving number of goroutines which we are going to wait.
 	wg.Add(2)
+
+	// Running both goroutines.
 	go GetEbayTags(ask)
 	go GetWildberriesTags(ask)
+
+	// Waiter waits at this part of code.
 	wg.Wait()
 }
 
+// Goroutines waiter.
 var wg sync.WaitGroup
 
 func main() {
+	// Setting timer.
 	start := time.Now()
+
+	// If string is given, using it, otherwise set "anime", because anime rules.
 	var ask string
-	if len(os.Args) == 2 {
+	if len(os.Args) > 1 {
 		ask = os.Args[1]
 	} else {
 		ask = "anime"
 	}
+
+	// Getting all tags.
 	GetTags(ask)
+
+	// Printing the total time elapsed.
 	fmt.Printf("%v\n", time.Since(start))
 }
--- main.py
+++ main.py
@@ -1,46 +1,69 @@
 import string
-import requests
-from bs4 import BeautifulSoup
+import sys
 import time
 
+import requests
+from bs4 import BeautifulSoup
 
+
+# Ebay.
 def get_tags_ebay(ask: str) -> list:
+    # Creating url using given request.
     url = f'https://ru.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw={ask}&_sacat=0'
+    # Creating request.
     response = requests.get(url)
+    # Creating page source .html document.
     soup = BeautifulSoup(response.text, 'lxml')
+    # Searching all needed div tags and all needed span tags.
     tags = list(map(lambda z: z.text, soup.find('div', class_='srp-related-searches').findAll('span', class_='BOLD')))
+    # Returning tags.
     return tags
 
 
 def get_tags_wildberries(ask: str) -> list:
+    # Creating url using given request.
     url = f'https://www.wildberries.ru/catalog/0/search.aspx?sort=popular&search={ask}/recommendation/catalog?type=maybeyouinterest'
+    # Creating request.
     response = requests.get(url)
+    # Creating page source .html document.
     soup = BeautifulSoup(response.text, 'lxml')
-    related = soup.findAll('div', id='mainContainer')
-    for cell in related:
-        print(cell.text)
-    print(related)
+    # related = soup.findAll('div', id='mainContainer')
+    # for cell in related:
+    #     print(cell.text)
+    # print(related)
 
     # TODO...
 
+    # Tags.
     tags = []
 
     # TODO...
 
+    # Returning tags.
     return tags
 
 
+# Getting all tags.
 def get_tags(ask: str) -> list:
     for symb in list(string.punctuation) + [' ']:
         ask = ask.replace(symb, '+')
     answer = []
-    #answer += get_tags_ebay(ask)
+    answer += get_tags_ebay(ask)
     answer = get_tags_wildberries(ask)
     return answer
 
 
 if __name__ == '__main__':
+    # Setting timer.
     start = time.time()
-    print(get_tags('аниме'))
+
+    # If string is given, using it, otherwise set "anime", because anime rules.
+    if len(sys.argv) > 1:
+        print(get_tags(sys.argv[1]))
+    else:
+        print(get_tags('anime'))
+
+    # Getting end time.
     finish = time.time()
+    # Printing the total time elapsed.
     print(str(int((finish - start) * 1000)) + 'ms')
